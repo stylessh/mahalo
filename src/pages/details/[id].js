4f704aff4dbd971b87b734429ed5b99ebc93ae1d
stylessh <stylesshjs@gmail.com>
2022-05-22 13:56:20 -0400

    fix: close button goes back in history

--- src/pages/details/[id].js
+++ src/pages/details/[id].js
@@ -48,28 +48,25 @@ const MovieDetails = ({ movie, related }) => {
 
         <section className="relative w-[90%] md:w-[70%] mx-auto z-20 mt-[90px] h-auto bg-dark border-2 border-gray-500 p-8 rounded-lg grid grid-cols-1 xl:grid-cols-[1fr,2fr] gap-x-5">
           {/* close button */}
-          <Link
-            href="/"
-            scroll
-            // onClick={() => router.back()}
+          <button
+            className="absolute top-5 right-6"
+            onClick={() => router.back()}
           >
-            <a className="absolute top-5 right-6">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-white"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </a>
-          </Link>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 text-white"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
 
           {/* media */}
           <article>
